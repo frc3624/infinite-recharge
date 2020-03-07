acfe40359b53088bcc6eeddc33425d63db3e8d22
Talha Masood <talhamasood1011@gmail.com>
2020-03-06 19:03:58 -0500

    limelight

--- src/main/java/frc/robot/subsystems/Limelight.java
+++ src/main/java/frc/robot/subsystems/Limelight.java
@@ -8,22 +8,70 @@
 package frc.robot.subsystems;
 
 import edu.wpi.first.networktables.NetworkTableInstance;
+import edu.wpi.first.wpilibj.Servo;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class Limelight extends SubsystemBase {
   /**
    * Creates a new Limelight.
    */
+  private Servo servo = new Servo(9);
+  private static final double UP_ANGLE = 225;
+  private static final double DOWN_ANGLE = 45;
+  private static final double FOWARD_ANGLE = 180;
   public Limelight() {
 
   }
 
-  public static double findDistance() {
+  public void lookFoward(){
+    servo.setAngle(FOWARD_ANGLE);
+  }
+
+  public void lookUp(){
+    servo.setAngle(UP_ANGLE);
+  }
+
+  public void lookDown(){
+    servo.setAngle(DOWN_ANGLE);
+  }
+  /**
+   * @return whether or not the limelight has any valid targets
+   */
+  public static double findValidTargets(){
+    return NetworkTableInstance.getDefault().getTable("limelight").getEntry("tv").getDouble(0);
+
+  }
+
+  /**
+   * @return The horizontal displacement from the limelight to the target
+   */
+  public static double findHorizontalDistance() {
     return NetworkTableInstance.getDefault().getTable("limelight").getEntry("tx").getDouble(0);
   }
+
+  /**
+   * @return the vertical displacement from the limelight to the target
+   */
+  public static double findVerticalDistance(){
+    return NetworkTableInstance.getDefault().getTable("limelight").getEntry("ty").getDouble(0);
+
+  }
+
+  /**
+   * @return the rotation from the limelight to the target
+   */
   public static double findAngle() {
     return -NetworkTableInstance.getDefault().getTable("limelight").getEntry("ts").getDouble(0);
   }
+  
+  /**
+   * @return the target area (0% to 100% of the image)
+   */
+  public static double findTargetArea() {
+    return NetworkTableInstance.getDefault().getTable("limelight").getEntry("ta").getDouble(0);
+  }
+
+
   @Override
   public void periodic() {
     // This method will be called once per scheduler run
