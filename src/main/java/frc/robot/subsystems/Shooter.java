c5a62a431c262a79f36d8fb867028b28a03ff914
Kyle Diaz <kyle@kylediaz.com>
2020-03-08 20:43:58 -0400

    added PID values Miles and I found for the shooter

--- src/main/java/frc/robot/subsystems/Shooter.java
+++ src/main/java/frc/robot/subsystems/Shooter.java
@@ -1,10 +1,3 @@
-/*----------------------------------------------------------------------------*/
-/* Copyright (c) 2019 FIRST. All Rights Reserved.                             */
-/* Open Source Software - may be modified and shared by FRC teams. The code   */
-/* must be accompanied by the FIRST BSD license file in the root directory of */
-/* the project.                                                               */
-/*----------------------------------------------------------------------------*/
-
 package frc.robot.subsystems;
 
 import static frc.robot.Constants.SHOOTER_MOTOR_ID;
@@ -12,40 +5,40 @@ import static frc.robot.Constants.SHOOTER_MOTOR_ID;
 import com.ctre.phoenix.motorcontrol.ControlMode;
 import com.ctre.phoenix.motorcontrol.can.WPI_TalonFX;
 
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class Shooter extends SubsystemBase {
+
   private final static double SHOOTER_WHEEL_RADIUS = 2.0 / 12.0;
-  private double kP = 1;
-  private double kI = 0;
-  private double kD = 0;
-  private final int MAXRPM = 5200;
-  private final WPI_TalonFX shoot = new WPI_TalonFX(SHOOTER_MOTOR_ID);
+  private final static int MAXRPM = 5200;
+
+  private final WPI_TalonFX shooterMotor = new WPI_TalonFX(SHOOTER_MOTOR_ID);
+  private final double kP = .135;
+  private final double kI = kP * .001;
+  private final double kD = kP * 20;
 
   public Shooter() {
-    /*
-    shoot.config_kP(0, gP);
-    shoot.config_kI(0, gI);
-    shoot.config_kD(0, gD);*/
+    shooterMotor.config_kP(0, kP);
+    shooterMotor.config_kI(0, kI);
+    shooterMotor.config_kD(0, kD);
   }
 
   @Override
   public void periodic() {
   }
 
-  /*public void displayOnShuffleboard() {
-    SmartDashboard.putNumber("Velocity", shoot.getSelectedSensorVelocity());
+  public void displayOnShuffleboard() {
+    SmartDashboard.putNumber("Velocity", shooterMotor.getSelectedSensorVelocity());
   }
 
   public void setLinearSpeed(double linearVelocity) {
-
     double rotationalVelocity = linearVelocity / SHOOTER_WHEEL_RADIUS;
-    shoot.set(ControlMode.Velocity, rotationalVelocity);
-
+    shooterMotor.set(ControlMode.Velocity, rotationalVelocity);
   }
 
-  */
   public void setShootMotorSpeed(double percent) {
-    shoot.set(ControlMode.PercentOutput, percent);
+    shooterMotor.set(ControlMode.PercentOutput, percent);
   }
+  
 }
