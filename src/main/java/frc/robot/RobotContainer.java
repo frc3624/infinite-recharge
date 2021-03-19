dd9060a283c083deaa509814cb68a32eef45bcdf
GeoDash897 <mattjroman03@gmail.com>
2021-03-19 16:10:26 -0400

    Properly implement shoot subsystem/commands/Shoot
    When initially programming the shooter for the robot, I accidently
    forgot to initialize the subsystem and command for the shooter.
    Whoops...

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -4,7 +4,9 @@ import edu.wpi.first.wpilibj.XboxController;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.button.JoystickButton;
 import frc.robot.commands.DriveTrain;
+import frc.robot.commands.Shoot;
 import frc.robot.subsystems.Drive;
+import frc.robot.subsystems.Shooter;
 
 public class RobotContainer {
   // The Buttons and Controllers
@@ -13,6 +15,8 @@ public class RobotContainer {
     
   // Subsystems
   private Drive drive;
+  private Shooter shooter;
+  private Shoot shoot;
   
   //Commands
   private DriveTrain driveTrain;
@@ -22,19 +26,22 @@ public class RobotContainer {
     configureSubsystems();
     configureCommands();
     drive.setDefaultCommand(driveTrain);
+    driver1ButtonA.whenHeld(shoot);
   }
 
   private void configureButtonBindings() {
     driver1 = new XboxController(0);
-    driver1ButtonA = new JoystickButton(driver1, 0);
+    driver1ButtonA = new JoystickButton(driver1, 1);
   }
 
   private void configureSubsystems() {
     drive = new Drive();
+    shooter = new Shooter();
   }
 
   private void configureCommands() {
     driveTrain = new DriveTrain(drive, driver1);
+    shoot = new Shoot(shooter);
   }
 
   public Command getAutonomousCommand() {
